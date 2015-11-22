55211fd5d8ef93971867b303cf9805aa37775b33
Seok Lee <seoklee@uark.edu>
2015-11-22 16:06:03 -0600

    does data cleaning in createdataset.py. Need to change the subreddit to what we discussed in the meeting.

--- project/scripts/populate_train_test_toy.py
+++ project/scripts/populate_train_test_toy.py
@@ -5,14 +5,19 @@ from sklearn.cross_validation import train_test_split
 dataset_path = '../data/subreddit_subset.csv'
 train_set_path = '../data/train_set.csv'
 test_set_path = '../data/test_set.csv'
-toy_set_path = '../data/toy_set.csv'
+toy_train_set_path = '../data/toy_train_set.csv'
+toy_test_set_path = '../data/toy_test_set.csv'
 
 df = pd.read_csv(dataset_path)
+r_train = np.random.RandomState(20)
+r_test = np.random.RandomState(30)
 r = np.random.RandomState(43)
 
 train_set, test_set = train_test_split(df, train_size=0.8, random_state=r)
-toy_set = train_set.sample(2000, random_state=r)
+toy_train_set = train_set.sample(2000, random_state=r_train)
+toy_test_set = train_set.sample(2000, random_state=r_test)
 
-train_set.to_csv(train_set_path, index=False)
-test_set.to_csv(test_set_path, index=False)
-toy_set.to_csv(toy_set_path, index=False)
+# train_set.to_csv(train_set_path, index=False)
+# test_set.to_csv(test_set_path, index=False)
+toy_train_set.to_csv(toy_train_set_path, index=False)
+toy_test_set.to_csv(toy_test_set_path, index=False)
