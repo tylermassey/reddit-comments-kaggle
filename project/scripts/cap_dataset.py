1e8f2750e2d830862ff6e6bdc68262caa307987a
Tyler Massey <jtmassey@uark.edu>
2015-12-06 14:48:02 -0600

    Modified cap_dataset.py file to randomly select comments instead of take the top N comments.
    Added two scripts to run naive bayes and random forest models. It runs
    several tests and reports the average accuracy.

--- project/scripts/cap_dataset.py
+++ project/scripts/cap_dataset.py
@@ -1,20 +1,23 @@
+import sys
+import numpy as np
 import pandas as pd
 
 dataset_path = '../data/subreddit_newsubset.csv'
 df = pd.read_csv(dataset_path)
 subreddit_names = df.subreddit.unique()
-
-limit = 50000
-
-capped_dataset_path = '../data/cappedset_' + str(limit) + '.csv'
-subsets = list()
+cap_size = int(sys.argv[1])
+num_outputs = int(sys.argv[2])
+r = np.random.RandomState(1234)
+output_file_part = '../data/cappedset_' + str(cap_size)
 
 df = pd.read_csv(dataset_path)
 
-for sr in subreddit_names:
-    sdf = df[df['subreddit'] == sr]
-    sdf = sdf[:limit]
-    subsets.append(sdf)
-
-df = pd.concat(subsets)
-df.to_csv(capped_dataset_path, index=False)
+for i in range(num_outputs):
+    subsets = list()
+    for sr in subreddit_names:
+        sdf = df[df['subreddit'] == sr]
+        sdf = sdf.sample(n=cap_size, random_state=r)
+        subsets.append(sdf)
+    capped_dataset_path = output_file_part + '_' + str(i) + '.csv'
+    df_2 = pd.concat(subsets)
+    df_2.to_csv(capped_dataset_path, index=False)
---
+++ project/scripts/tyler-rf-avg.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+import pandas as pd
+import gc
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+
+names = ['nba', 'relationships', 'todayilearned', 'leagueoflegends', 'AdviceAnimals', 'news', 'movies', 'nfl',
+         'CasualConversation', 'hockey']
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+cap_size = int(sys.argv[1])
+num_files = int(sys.argv[2])
+output_file_part = '../data/cappedset_' + str(cap_size) + '_'
+
+results = list()
+
+for i in range(num_files):
+    capped_file = output_file_part + str(i) + '.csv'
+    r = np.random.RandomState(43)
+
+    df = pd.read_csv(capped_file)
+    train_df, test_df = train_test_split(df, train_size=0.95, random_state=r)
+    del df
+    gc.collect()
+
+    text_clf = Pipeline([('vect', CountVectorizer(max_features=10000)),
+                         ('tfidf', TfidfTransformer(norm='l2', sublinear_tf=True)),
+                         ('clf', RandomForestClassifier(n_estimators=40))])
+    text_clf = text_clf.fit(train_df.body, train_df.subreddit_index)
+    predicted = text_clf.predict(test_df.body)
+    results.append(np.mean(predicted == test_df.subreddit_index))
+    print "capped file " + str(i) + " : " + str(results[i])
+
+average = np.average(results)
+print "average = " + str(average)
